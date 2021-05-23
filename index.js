314bd5eade3d9e71dcdc3be736596fa26ae680d7
Ben Spotswood <spotswoodb@gmail.com>
2021-05-23 18:09:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here!
+
+// let element = document.createElement('div') 
+
+
+// document.body.appendChild(element);
+
+// let ul = document.createElement('ul')
+
+// for (let i = 0; i < 3; i++) {
+//     let li = document.createElement('li')
+//     li.innerHTML = (i + 1).toString()
+//     ul.appendChild(li)
+// }
+
+// element.appendChild(ul);
+
+// element.style.textAlign = 'center';
+// ul.style.textAlign = 'left'; 
+
+// element.style.backgroundColor = '#27647B';
+
+document.getElementById('main').remove();
+
+var newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "BEN is the champion";
+
+document.body.appendChild(newHeader);
