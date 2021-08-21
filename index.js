355ab0d96bae7d99cb2dd06f2fc79d616350822d
Ben Spotswood <spotswoodb@gmail.com>
2021-08-20 22:56:08 -0500

    commit tree 7c8af83985dd983ea67946363f5f80e3524feab6

--- index.js
+++ index.js
@@ -2,10 +2,20 @@
 
 // let element = document.createElement('div') 
 
+let element = document.createElement('div');
+document.body.appendChild(element);
 
 // document.body.appendChild(element);
-
+let ul = document.createElement('ul')
 // let ul = document.createElement('ul')
+for (let i = 0; i < 3; i++){
+    let li = document.createElement('li')
+    li.innerHTML = (i + 1).toString()
+    ul.appendChild(li)
+}
+
+element.appendChild(ul);
+
 
 // for (let i = 0; i < 3; i++) {
 //     let li = document.createElement('li')
@@ -15,17 +25,27 @@
 
 // element.appendChild(ul);
 
-// element.style.textAlign = 'center';
-// ul.style.textAlign = 'left'; 
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left'; 
 
-// element.style.backgroundColor = '#27647B';
+element.style.backgroundColor = '#27647B';
 
-document.getElementById('main').remove();
+ul.removeChild(ul.querySelector('li:nth-child(2)'))
+ul.remove()
+// document.getElementById('main').remove();
 
-var newHeader = document.createElement('h1');
+// var newHeader = document.createElement('h1');
 
-newHeader.setAttribute("id", "victory");
+// newHeader.setAttribute("id", "victory");
 
-newHeader.innerHTML = "BEN is the champion";
+// newHeader.innerHTML = "BEN is the champion";
 
-document.body.appendChild(newHeader);
+// document.body.appendChild(newHeader);
+
+document.getElementById('main').remove()
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id", "victory")
+
+const name = "Ben"
+newHeader.innerHTML = `${name} is the champion`
